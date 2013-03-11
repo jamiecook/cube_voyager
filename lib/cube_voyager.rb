44222aadb09a5786f76bd73ff56433e767b16532
jamie.cook <jamie@ieee.org>
2013-03-11 12:20:55 +1000

    finalised matrix reading functions

--- lib/cube_voyager.rb
+++ lib/cube_voyager.rb
@@ -2,17 +2,26 @@ require 'rubygems'
 require 'cube_voyager/version'
 require 'ffi'
 
+class FFI::Pointer
+  def read_array_of_strings(size)
+    return (0..(size-1)).map { |i| 
+      str_pointer = self + i * FFI::Type::POINTER.size 
+      str_pointer.read_pointer.read_string 
+    }
+  end
+end
+
 module CubeVoyager
   extend FFI::Library
   ffi_lib 'd:/bdk/source/cube_voyager/lib/VoyagerFileAccess.dll'
-  # void* MatReaderOpen(const char *filename, char *errMsg, int errBufLen);
+  #          void* MatReaderOpen(const char *filename, char *errMsg, int errBufLen);
   attach_function :MatReaderOpen,        [ :string, :pointer, :int ],       :pointer
+
   attach_function :MatReaderGetNumMats,  [ :pointer ],                      :int
   attach_function :MatReaderGetNumZones, [ :pointer ],                      :int
   attach_function :MatReaderClose,       [ :pointer ],                      :void
   attach_function :MatReaderGetRow,      [ :pointer, :int, :int, :pointer], :int
-  #attach_function :MatReaderGetMatrixNames, [ :pointer, (Pointer state, String[] names) ], :int
-  #attach_function :MatReaderGetRow(Pointer state, int MatNumber, int RowNumber, double[] buffer), :int
+  attach_function :MatReaderGetMatrixNames, [ :pointer, :pointer], :int
 
   def self.read_csv_matrix(filename)
     IO.readlines(filename).drop(1).map { |line|
@@ -22,7 +31,8 @@ module CubeVoyager
 
   def self.open_matrix_reader(filename)
     err_buffer = FFI::MemoryPointer.from_string('-' * 512)
-    handle = MatReaderOpen(filename, err_buffer, 255)
+    # filename.gsub('/', '\\')
+    handle = MatReaderOpen(filename, err_buffer, 512)
     error_string = err_buffer.read_string.inspect
     raise "Error: Couldn't open #{filename}, because [#{error_string}]"  if handle.null?
     handle
@@ -32,15 +42,28 @@ module CubeVoyager
     MatReaderClose(handle)
   end
 
+  def self.get_number_of_matrices(handle)
+    return MatReaderGetNumMats(handle)
+  end
+
+  def self.get_number_of_zones(handle)
+    return MatReaderGetNumZones(handle)
+  end
+
   def self.read_matrix(handle, matrix_number)
     num_zones = MatReaderGetNumZones(handle)
-    1..num_zones.map { |i|
+    (1..num_zones).map { |i|
       buffer = FFI::MemoryPointer.new(:double, num_zones)
       read = MatReaderGetRow(handle, matrix_number, i, buffer)
-      p read
-      p buffer.inspect
       buffer.get_array_of_double(0, num_zones)
     }
+  end
+
+  def self.read_names(handle)
+    num_matrices = get_number_of_matrices(handle)
+    buffer       = FFI::MemoryPointer.new(:string, num_matrices)
+    MatReaderGetMatrixNames(handle, buffer)
+    return buffer.read_array_of_strings(num_matrices)
   end
 end
 
--- test/test_matrix.rb
+++ test/test_matrix.rb
@@ -21,15 +21,48 @@ class CubeVoyagerMatrixTest < Test::Unit::TestCase
     }
   end
 
+  def test_matrix_count
+    assert_nothing_raised {
+      handle = CubeVoyager::open_matrix_reader(File.join(@sample_data_dir, 'small_matrix.MAT'))
+      num_mats = CubeVoyager::get_number_of_matrices(handle)
+      assert_equal(10, num_mats)
+      CubeVoyager::close_matrix_reader(handle)
+    }
+  end
+
+  def test_num_zones
+    assert_nothing_raised {
+      handle = CubeVoyager::open_matrix_reader(File.join(@sample_data_dir, 'small_matrix.MAT'))
+      num_mats = CubeVoyager::get_number_of_zones(handle)
+      assert_equal(5, num_mats)
+      CubeVoyager::close_matrix_reader(handle)
+    }
+  end
+
+  def test_names
+    assert_nothing_raised {
+      handle = CubeVoyager::open_matrix_reader(File.join(@sample_data_dir, 'small_matrix.MAT'))
+      names = CubeVoyager::read_names(handle)
+      expected_names = %w(DT_AVE DF_AVE D1_AVE D2_AVE D3_AVE D4_AVE D23_AVE D32_AVE D24_AVE D42_AVE)
+      assert_equal(expected_names, names)
+      CubeVoyager::close_matrix_reader(handle)
+    }
+  end
+
   def test_matrix_read
     assert_nothing_raised {
       handle = CubeVoyager::open_matrix_reader(File.join(@sample_data_dir, 'small_matrix.MAT'))
-      mat = CubeVoyager::read_matrix(handle, 0)
+      mat = CubeVoyager::read_matrix(handle, 1)
       assert_kind_of(Array, mat)
       assert_equal(5, mat.size)
       assert_kind_of(Array, mat.first)
       assert_equal(5, mat.first.size)
       CubeVoyager::close_matrix_reader(handle)
+
+      mat_csv = CubeVoyager::read_csv_matrix(File.join(@sample_data_dir, 'small_matrix.csv'))
+      (0..4).each { |i| (0..4).each { |j| 
+        assert_equal(mat_csv[i][j], mat[i][j])
+      }}
     }
   end
 
